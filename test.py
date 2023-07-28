307e0fb6fa357f33e3b61de8390c3934959b3342
faizalfeng <fzx401@gmail.com>
2023-07-28 22:55:17 +0800

    2023.7.28

--- test.py
+++
@@ -1,7 +0,0 @@
-from LinkedList.linkedList import *
-
-
-linked_list = LinkList([1, 2, 3, 4, 5])
-
-# new_linked_list = linked_list.reverseList()
-pass
--- test2.py
+++
@@ -1,1 +0,0 @@
-from LinkedList.linkedList import LinkList
